6cccf58c93c70226d2e0b2a0f0f6c2033ed56356
TheSecondCore <martin.bagniuk@gmail.com>
2019-06-23 16:24:32 +0300

    Updates

---
+++ precache-manifest.a460b1d6a7d3f9e07c07b6436caa2da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ccb02c5c1175f978a9d9321e4b3c83",
+    "url": "/BagniukMHillerReact/index.html"
+  },
+  {
+    "revision": "6540fcd5a3a5121994cb",
+    "url": "/BagniukMHillerReact/static/css/main.adcdc6a7.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/BagniukMHillerReact/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "6540fcd5a3a5121994cb",
+    "url": "/BagniukMHillerReact/static/js/main.b62c5d7e.chunk.js"
+  },
+  {
+    "revision": "5f9490e208e72e7eb6f3",
+    "url": "/BagniukMHillerReact/static/js/runtime~main.948cbd6f.js"
+  }
+]);
